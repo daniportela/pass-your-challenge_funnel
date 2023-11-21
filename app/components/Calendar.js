b388527eccb49ec4092d89b5557a0f0b7b5a7f71
Dani Portela <daniel.portela96@hotmail.com>
2023-11-21 16:48:44 -0300

    Small text size changes

--- app/components/Calendar.js
+++ app/components/Calendar.js
@@ -6,7 +6,7 @@ export default function Calendar() {
     return (
         <section id="book-call" className="book-call d-grid grid-cols-1 max-width">
             <span className="fs-400 fw-300 text-center text-blue text-uppercase d-block">¿Te queda alguna duda?</span>
-            <h1 className="fs-700 fw-800 text-center">Nuestro equipo está listo para ayudarte</h1>
+            <h3 className="fs-700 fw-800 text-center">Nuestro equipo está listo para ayudarte</h3>
             <p className="fs-400 text-center">
                 <strong>¿Listo para desbloquear tu potencial financiero?</strong> Agenda una reunión con nuestros especialistas en inversiones.
                 Estamos ansiosos por trabajar contigo y proporcionarte las herramientas y estrategias necesarias para alcanzar
